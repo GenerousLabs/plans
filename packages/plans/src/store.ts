4a095031bb1821acd50f582e212e7c19b149cbe5
Callum Macdonald <git@callum-macdonald.com>
2020-05-22 16:51:07 +0200

    Add server side redux devtools.

--- packages/plans/src/store.ts
+++ packages/plans/src/store.ts
@@ -1,4 +1,5 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import devToolsEnhancer from 'remote-redux-devtools';
 
 import storage from './services/storage/storage.service';
 
@@ -6,6 +7,10 @@ export const store = configureStore({
   reducer: {
     storage,
   },
+  devTools: false,
+  enhancers: [
+    devToolsEnhancer({ hostname: 'localhost', port: 8000, realtime: true }),
+  ],
 });
 
 export type RootState = ReturnType<typeof store.getState>;
