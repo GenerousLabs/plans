529c547cbda623e014369c23e2d99470baeea138
Callum Macdonald <git@callum-macdonald.com>
2020-05-29 11:11:04 +0200

    Only create store on demand. New startup.

--- packages/plans/src/index.ts
+++ packages/plans/src/index.ts
@@ -1,11 +1,33 @@
-import { store } from './store';
+import { promises as fs } from 'fs';
+import { join } from 'path';
+import { loadPlansFromUserPath } from './services/plans/plans.actions';
+import { init as initStorage } from './services/storage/storage.service';
+import { AppThunk, createStore } from './store';
 
-import { init } from './services/storage/storage.service';
-
-export const start = async () => {
-  console.log('Start #OlwQwc');
-
-  store.dispatch(init());
+export const start = ({
+  rootPath,
+}: {
+  rootPath: string;
+}): AppThunk => async dispatch => {
+  dispatch(initStorage());
+  dispatch(
+    loadPlansFromUserPath({
+      fs,
+      userDirectoryPath: join(rootPath, 'bob'),
+      userId: 'bob',
+    })
+  );
 };
 
-start();
+export const startWithPackageStore = ({ rootPath }: { rootPath: string }) => {
+  const store = createStore();
+  store.dispatch(start({ rootPath }));
+};
+
+if (process.env.NODE_ENV === 'development') {
+  if (typeof process.env.ROOT_PATH === 'string') {
+    const { ROOT_PATH } = process.env;
+    console.log('Starting with ROOT_PATH #2Yc8PN', ROOT_PATH);
+    startWithPackageStore({ rootPath: ROOT_PATH });
+  }
+}
--- packages/plans/src/store.ts
+++ packages/plans/src/store.ts
@@ -1,30 +1,52 @@
 import {
-  configureStore,
-  ThunkAction,
   Action,
   combineReducers,
+  configureStore,
+  ThunkAction,
 } from '@reduxjs/toolkit';
-import devToolsEnhancer from 'remote-redux-devtools';
-
-import storage from './services/storage/storage.service';
+import devToolsEnhancer, {
+  RemoteReduxDevToolsOptions,
+} from 'remote-redux-devtools';
 import plans from './services/plans/plans.state';
+import storage from './services/storage/storage.service';
 
-const REDUX_ROOT_KEY = '__plans' as const;
+export const REDUX_ROOT_KEY = '__plans' as const;
 
 const reducer = combineReducers({
   storage,
   plans,
 });
 
-export const store = configureStore({
-  reducer: {
-    [REDUX_ROOT_KEY]: reducer,
-  },
-  devTools: false,
-  enhancers: [
-    devToolsEnhancer({ hostname: 'localhost', port: 8000, realtime: true }),
-  ],
-});
+export const createStore = (args?: {
+  enableDevTools?: boolean;
+  enableRemoteDevTools?: boolean;
+  devToolsEnhancerArgs?: RemoteReduxDevToolsOptions;
+}) => {
+  const {
+    enableDevTools = false,
+    enableRemoteDevTools = true,
+    devToolsEnhancerArgs = {
+      hostname: 'localhost',
+      port: 8000,
+      realtime: true,
+    },
+  } = typeof args === 'undefined' ? {} : args;
+
+  const enhancers = [];
+  if (enableRemoteDevTools) {
+    enhancers.push(devToolsEnhancer(devToolsEnhancerArgs));
+  }
+
+  const store = configureStore({
+    reducer: {
+      [REDUX_ROOT_KEY]: reducer,
+    },
+    devTools: enableDevTools,
+    enhancers,
+  });
+
+  return store;
+};
 
 // export type RootState = ReturnType<typeof store.getState>;
 export type RootState = {
