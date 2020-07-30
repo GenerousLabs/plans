039a85b1ced0e7759cb3bc4786357d601a582128
Callum Macdonald <git@callum-macdonald.com>
2020-07-30 18:43:56 +0200

    Load the plans reducer.

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,8 +1,10 @@
 import { Action, configureStore, ThunkAction } from "@reduxjs/toolkit";
+import { reducer as plansReducer, REDUX_ROOT_KEY as plansKey } from "plans";
 import counterReducer from "./features/counter/counterSlice";
 
 export const store = configureStore({
   reducer: {
+    [plansKey]: plansReducer,
     counter: counterReducer,
   },
 });
