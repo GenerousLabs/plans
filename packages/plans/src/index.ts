640dfc7d49152671a28d133cfbcd15cc9cd1d29a
Callum Macdonald <git@callum-macdonald.com>
2020-07-30 18:43:39 +0200

    Export redux key.

--- packages/plans/src/index.ts
+++ packages/plans/src/index.ts
@@ -3,3 +3,4 @@ import './services/startup/startup.service';
 
 export { startup } from './services/startup/actions/startup.action';
 export { reducer } from './store';
+export { REDUX_ROOT_KEY } from './shared.constants';
