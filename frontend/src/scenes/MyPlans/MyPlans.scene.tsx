5d9ed0fcdc587c3499d6eaa8e904ad2fe2dbd801
Callum Macdonald <git@callum-macdonald.com>
2020-07-31 19:06:27 +0200

    Render very simple list of my plans.

--- frontend/src/scenes/MyPlans/MyPlans.scene.tsx
+++ frontend/src/scenes/MyPlans/MyPlans.scene.tsx
@@ -1,7 +1,24 @@
+import { Typography } from "@material-ui/core";
+import { selectAllMyPlans } from "plans";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const MyPlans = () => {
-  return <div>MyPlans</div>;
+  const myPlans = useSelector(selectAllMyPlans);
+
+  return (
+    <div>
+      <Typography variant="h1">My Plans</Typography>
+      <Typography>
+        This app is a work in progress. Please bear with us while we improve it.
+      </Typography>
+      <ul>
+        {myPlans.map((plan) => (
+          <li key={plan.id}>{plan.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default MyPlans;
