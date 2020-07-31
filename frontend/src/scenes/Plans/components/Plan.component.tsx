2b1d8dafc711a6d3b5bc48b019dbe16f0b8d0f58
Callum Macdonald <git@callum-macdonald.com>
2020-07-31 14:43:09 +0200

    Render repo names.

--- frontend/src/scenes/Plans/components/Plan.component.tsx
+++ frontend/src/scenes/Plans/components/Plan.component.tsx
@@ -1,21 +1,39 @@
-import { Typography } from "@material-ui/core";
-import { selectPlanWithUserOrThrow } from "plans";
+import {
+  createStyles,
+  Divider,
+  makeStyles,
+  Typography,
+} from "@material-ui/core";
+import { selectPlanWithUserAndRepoOrThrow } from "plans";
 import React from "react";
 import { useSelector } from "react-redux";
 import { AppState } from "../../../store";
 
 const Plan = ({ id }: { id: string }) => {
-  const { plan, user } = useSelector((state: AppState) =>
-    selectPlanWithUserOrThrow(state, id)
+  const classes = useStyles();
+  const { plan, user, repo } = useSelector((state: AppState) =>
+    selectPlanWithUserAndRepoOrThrow(state, id)
   );
 
   return (
     <div>
-      <Typography variant="h3">{user.name}</Typography>
+      <Typography variant="h5" component="h3">
+        Repo: {repo.name}
+      </Typography>
       <Typography variant="h2">{plan.name}</Typography>
       <Typography>{plan.description}</Typography>
+      <Divider className={classes.divider} />
     </div>
   );
 };
 
 export default Plan;
+
+const useStyles = makeStyles((theme) =>
+  createStyles({
+    divider: {
+      marginTop: theme.spacing(3),
+      marginBottom: theme.spacing(3),
+    },
+  })
+);
