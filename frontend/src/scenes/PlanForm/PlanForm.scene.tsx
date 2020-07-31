0807bc39aab3c1431a3ab0b8a82d1cef828c51f3
Callum Macdonald <git@callum-macdonald.com>
2020-07-31 15:31:46 +0200

    Sketch out a basic form.

--- frontend/src/scenes/PlanForm/PlanForm.scene.tsx
+++ frontend/src/scenes/PlanForm/PlanForm.scene.tsx
@@ -1,7 +1,56 @@
 import React from "react";
+import { Typography, Button } from "@material-ui/core";
+import { Field, Form, Formik } from "formik";
+import * as yup from "yup";
+import { TextField } from "formik-material-ui";
+
+const schema = yup.object().shape({
+  name: yup.string().required(),
+  description: yup.string(),
+});
+
+const initialValues = {
+  name: "",
+  description: "",
+};
 
 const PlanForm = () => {
-  return <div>PlanForm</div>;
+  return (
+    <div>
+      <Typography variant="h1">New Plan</Typography>
+      <Typography>
+        Please bear with us while we develop this. It's very much a work in
+        progress.
+      </Typography>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={schema}
+        onSubmit={() => {
+          debugger;
+        }}
+      >
+        <Form>
+          <Field
+            name="name"
+            label="Plan name"
+            component={TextField}
+            fullWidth
+          />
+          <Field
+            name="description"
+            label="Plan description"
+            helperText="Supports markdown"
+            component={TextField}
+            fullWidth
+            rows={4}
+          />
+          <Button variant="contained" type="submit">
+            Save Plan
+          </Button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 export default PlanForm;
