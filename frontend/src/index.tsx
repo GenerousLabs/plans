71a5e82b8f02424de50fe896624da99c744ab959
Callum Macdonald <git@callum-macdonald.com>
2020-07-31 12:31:56 +0200

    Prettier.

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import { store } from "./store";
 import { Provider } from "react-redux";
+import App from "./App";
+import "./index.css";
 import * as serviceWorker from "./serviceWorker";
+import { store } from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
