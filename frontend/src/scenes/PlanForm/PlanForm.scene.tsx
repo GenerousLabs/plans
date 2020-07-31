71589c5f85dfcd40ca506d098e50fa6f890fe06a
Callum Macdonald <git@callum-macdonald.com>
2020-07-31 18:53:39 +0200

    Use the new action. Works.

--- frontend/src/scenes/PlanForm/PlanForm.scene.tsx
+++ frontend/src/scenes/PlanForm/PlanForm.scene.tsx
@@ -1,8 +1,11 @@
-import React from "react";
-import { Typography, Button } from "@material-ui/core";
+import { Button, Typography } from "@material-ui/core";
 import { Field, Form, Formik } from "formik";
-import * as yup from "yup";
 import { TextField } from "formik-material-ui";
+import React from "react";
+import { useDispatch } from "react-redux";
+import * as yup from "yup";
+import { AppDispatch, addPlanConfigs } from "../../store";
+import { createNewMyPlan } from "plans";
 
 const schema = yup.object().shape({
   name: yup.string().required(),
@@ -15,6 +18,8 @@ const initialValues = {
 };
 
 const PlanForm = () => {
+  const dispatch: AppDispatch = useDispatch();
+
   return (
     <div>
       <Typography variant="h1">New Plan</Typography>
@@ -25,8 +30,20 @@ const PlanForm = () => {
       <Formik
         initialValues={initialValues}
         validationSchema={schema}
-        onSubmit={() => {
-          debugger;
+        onSubmit={(values, helpers) => {
+          const { name, description } = values;
+          dispatch(
+            createNewMyPlan(
+              addPlanConfigs({
+                plan: {
+                  name,
+                  description,
+                },
+              })
+            )
+          );
+
+          helpers.resetForm();
         }}
       >
         <Form>
