07cff46aa4e2e06be2606955a955e5b6e6300a76
Callum Macdonald <git@callum-macdonald.com>
2020-08-25 18:46:38 +0200

    Skeleton connections scene.

--- frontend/src/scenes/Routes/Routes.scene.tsx
+++ frontend/src/scenes/Routes/Routes.scene.tsx
@@ -5,10 +5,11 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Route, Router, Switch } from "react-router-dom";
 import { AppState } from "../../store";
+import Bar from "../Bar/Bar.scene";
+import Connections from "../Connections/Connections.scene";
 import MyPlans from "../MyPlans/MyPlans.scene";
 import PlanForm from "../PlanForm/PlanForm.scene";
 import Plans from "../Plans/Plans.scene";
-import Bar from "../Bar/Bar.scene";
 
 export const history = createBrowserHistory();
 
@@ -33,6 +34,7 @@ const Routes = () => {
         {hasLoaded ? (
           <Switch>
             <Route path="/" exact component={Plans} />
+            <Route path="/connections" exact component={Connections} />
             <Route path="/plans/mine" exact component={MyPlans} />
             <Route path="/plans/new" exact component={PlanForm} />
           </Switch>
