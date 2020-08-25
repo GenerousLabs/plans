de9fe189e864aa181dac3562868dc39634e20330
Callum Macdonald <git@callum-macdonald.com>
2020-08-25 16:40:32 +0200

    Experiments with node-git-server.

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,1 +1,52 @@
-console.log("Server started #fK5QX6");
+import Server from "node-git-server";
+import logger from "./util/logger";
+import path from "path";
+
+const PORT = parseInt(process.env.PORT || "8000");
+
+const repos = new Server(path.join(__dirname, "../data/repos"), {
+  autoCreate: true,
+  authenticate: (params) => {
+    return new Promise((resolve, reject) => {
+      params.user((user, pass) => {
+        logger.debug("authenticate #sI3laF", {
+          params,
+          auth: { user, pass },
+        });
+        if (user !== "bob") {
+          return reject();
+        }
+        return resolve();
+      });
+    });
+  },
+});
+
+/*
+repos.on("fetch", (fetch) => {
+  logger.debug("fetch #tgbL2b", fetch);
+  fetch.reject();
+});
+
+repos.on("head", (head) => {
+  head.reject();
+});
+
+repos.on("info", (info) => {
+  logger.debug("info #lBbbLL");
+  info.reject();
+});
+*/
+
+repos.on("push", (push) => {
+  logger.debug("push #tgbL2b", push);
+  push.accept();
+});
+
+repos.on("tag", (tag) => {
+  tag.reject();
+});
+
+repos.listen(PORT, () => {
+  logger.debug(`Listening on :${PORT}`);
+});
---
+++ server/src/modules.d.ts
@@ -0,0 +1,47 @@
+declare module "node-git-server" {
+  type EventName =
+    // | 'close'
+    // | 'data'
+    // | 'drain'
+    // | 'end'
+    // | 'error'
+    "fetch" | "head" | "info" | "push" | "tag";
+
+  class HttpDuplex {}
+
+  class Service {
+    accept: () => void;
+    reject: () => void;
+  }
+
+  class Server {
+    constructor(
+      path: string,
+      opts?: {
+        autoCreate?: boolean;
+        authenticate?: (params: {
+          type: string;
+          repo: string;
+          /**
+           * NOTE: Invoking the `user()` function will cause any requests
+           * without a Basic authentication header to return a 401. This
+           * triggers git to request a username / password from the user.
+           */
+          user: (
+            callback: (username: string, password: string, error: null) => void
+          ) => void;
+          headers: { [x: string]: string };
+        }) => Promise<void>;
+        /**
+         * If `opts.checkout` is true, create and expected checked-out repos instead of bare repos
+         */
+        checkout?: boolean;
+      }
+    );
+    // TODO: Better typing of `action` here
+    on(event: EventName, callback: (action: Service) => void): void;
+    list(callback: (err: Error, results: string[]) => void): void;
+    listen(port: number, callback?: () => void): void;
+  }
+  export default Server;
+}
