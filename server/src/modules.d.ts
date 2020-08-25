880b0cd70157a4bbfcd22f12c64d6160a62be69d
Callum Macdonald <git@callum-macdonald.com>
2020-08-25 16:53:09 +0200

    Expand types.

--- server/src/modules.d.ts
+++ server/src/modules.d.ts
@@ -19,19 +19,31 @@ declare module "node-git-server" {
       path: string,
       opts?: {
         autoCreate?: boolean;
-        authenticate?: (params: {
-          type: string;
-          repo: string;
-          /**
-           * NOTE: Invoking the `user()` function will cause any requests
-           * without a Basic authentication header to return a 401. This
-           * triggers git to request a username / password from the user.
-           */
-          user: (
-            callback: (username: string, password: string, error: null) => void
-          ) => void;
-          headers: { [x: string]: string };
-        }) => Promise<void>;
+        /**
+         * If authenticate() returns a promise, it will be awaited. If not, the
+         * `next()` callback should be invoked without an `error` to signal
+         * success, or with an `error` to signal failure.
+         */
+        authenticate?: (
+          params: {
+            type: string;
+            repo: string;
+            /**
+             * NOTE: Invoking the `user()` function will cause any requests
+             * without a Basic authentication header to return a 401. This
+             * triggers git to request a username / password from the user.
+             */
+            user: (
+              callback: (
+                username: string,
+                password: string,
+                error: null
+              ) => void
+            ) => void;
+            headers: { [x: string]: string };
+          },
+          next: (error?: Error) => void
+        ) => Promise<void> | void;
         /**
          * If `opts.checkout` is true, create and expected checked-out repos instead of bare repos
          */
