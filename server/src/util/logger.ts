3bda25c6782f1e0dd5679ba5efd4886486f87303
Callum Macdonald <git@callum-macdonald.com>
2020-08-27 19:55:45 +0200

    Better logging.

--- server/src/util/logger.ts
+++ server/src/util/logger.ts
@@ -4,6 +4,11 @@ const options: winston.LoggerOptions = {
   transports: [
     new winston.transports.Console({
       level: process.env.NODE_ENV === "production" ? "error" : "debug",
+      format: winston.format.combine(
+        winston.format.metadata(),
+        winston.format.prettyPrint(),
+        winston.format.colorize()
+      ),
     }),
     new winston.transports.File({ filename: "debug.log", level: "debug" }),
   ],
