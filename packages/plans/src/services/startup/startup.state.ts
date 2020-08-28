5582a3fd238c6062c6fdf7ddfff79d062c06e891
Callum Macdonald <git@callum-macdonald.com>
2020-08-28 18:23:25 +0200

    Remove username from startup state.

--- packages/plans/src/services/startup/startup.state.ts
+++ packages/plans/src/services/startup/startup.state.ts
@@ -1,20 +1,15 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { RootState } from '../../store';
-import { REDUX_ROOT_KEY } from '../../shared.constants';
+import { createSlice } from '@reduxjs/toolkit';
 
 export const REDUCER_KEY = 'startup' as const;
 
 type StartupState = {
   initStarted: boolean;
   initCompleted: boolean;
-  myUsername?: string;
 };
 
 const initialState: StartupState = {
   initStarted: false,
   initCompleted: false,
-  // TODO Update the username
-  myUsername: 'alice',
 };
 
 const startupSlice = createSlice({
@@ -27,22 +22,9 @@ const startupSlice = createSlice({
     initFinished: (state) => {
       state.initCompleted = true;
     },
-    setUsername: (state, action: PayloadAction<string>) => {
-      state.myUsername = action.payload;
-    },
   },
 });
 
 export const { initFinished, initStarted } = startupSlice.actions;
 
 export default startupSlice.reducer;
-
-export const getMyUsername = (state: RootState) =>
-  state[REDUX_ROOT_KEY][REDUCER_KEY].myUsername;
-export const getMyUsernameOrThrow = (state: RootState) => {
-  const myUsername = getMyUsername(state);
-  if (typeof myUsername !== 'string') {
-    throw new Error('Failed to get my username. #4Qv0OI');
-  }
-  return myUsername;
-};
