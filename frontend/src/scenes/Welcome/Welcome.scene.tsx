1798dd2f297b90ef88f2bd5b6defba9317e71eb8
Callum Macdonald <git@callum-macdonald.com>
2020-08-31 22:02:51 +0200

    Updated welcome screen.

--- frontend/src/scenes/Welcome/Welcome.scene.tsx
+++ frontend/src/scenes/Welcome/Welcome.scene.tsx
@@ -12,7 +12,9 @@ const Welcome = ({ loading }: { loading: boolean }) => {
 
   return (
     <div>
-      <Typography variant="h1">Welcome</Typography>
+      <Typography variant="h1" component="h2">
+        Welcome
+      </Typography>
       <Typography>
         Share your digital plans (accounts) with people you love and trust.
       </Typography>
@@ -21,6 +23,20 @@ const Welcome = ({ loading }: { loading: boolean }) => {
         <strong>test server</strong>. Any data you enter here might become
         public, and might be lost at any time. This is very experimental
         software!
+      </Typography>
+      <Typography variant="h3">Getting started</Typography>
+      <Typography>Follow these steps to get started.</Typography>
+      <Typography>1: Tell Callum what username you would like.</Typography>
+      <Typography>
+        2: Once Callum sends you an activation code, reload this page.
+      </Typography>
+      <Typography>3: Enter the activation code.</Typography>
+      <Typography>
+        4: Go to the "My Plans" section, click "Add a plan", and add some plans.
+      </Typography>
+      <Typography>
+        5: Send the sharing code from the My Plans page to friends who also have
+        an account.
       </Typography>
       {loading ? (
         <div className={classes.loadingWrapper}>
