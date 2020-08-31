df0d1311e284ef79a5afa3f1fcc5ab6dd60853bb
Callum Macdonald <git@callum-macdonald.com>
2020-08-31 21:58:56 +0200

    Show welcome during loading screen.

--- frontend/src/scenes/Routes/Routes.scene.tsx
+++ frontend/src/scenes/Routes/Routes.scene.tsx
@@ -1,4 +1,4 @@
-import { CircularProgress, Container } from "@material-ui/core";
+import { Container } from "@material-ui/core";
 import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
 import { createBrowserHistory } from "history";
 import React from "react";
@@ -13,15 +13,6 @@ import Plans from "../Plans/Plans.scene";
 import Welcome from "../Welcome/Welcome.scene";
 
 export const history = createBrowserHistory();
-
-const Loading = () => {
-  const classes = useStyles();
-  return (
-    <div className={classes.loadingWrapper}>
-      <CircularProgress size="6rem" />
-    </div>
-  );
-};
 
 const Routes = () => {
   const hasLoaded = useSelector(
@@ -41,7 +32,7 @@ const Routes = () => {
             <Route path="/plans/new" exact component={PlanForm} />
           </Switch>
         ) : (
-          <Loading />
+          <Welcome loading={!hasLoaded} />
         )}
       </Container>
     </Router>
@@ -49,22 +40,3 @@ const Routes = () => {
 };
 
 export default Routes;
-
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    container: {
-      flexGrow: 1,
-    },
-    loadingWrapper: {
-      width: "100vw",
-      height: "100vh",
-      position: "absolute",
-      top: 0,
-      left: 0,
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-      zIndex: -1,
-    },
-  })
-);
--- frontend/src/scenes/Welcome/Welcome.scene.tsx
+++ frontend/src/scenes/Welcome/Welcome.scene.tsx
@@ -1,7 +1,55 @@
+import {
+  CircularProgress,
+  createStyles,
+  makeStyles,
+  Theme,
+  Typography,
+} from "@material-ui/core";
 import React from "react";
 
-const Welcome = () => {
-  return <div>Welcome</div>;
+const Welcome = ({ loading }: { loading: boolean }) => {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Typography variant="h1">Welcome</Typography>
+      <Typography>
+        Share your digital plans (accounts) with people you love and trust.
+      </Typography>
+      <Typography>
+        Right now this is a <strong>private beta</strong> and a{" "}
+        <strong>test server</strong>. Any data you enter here might become
+        public, and might be lost at any time. This is very experimental
+        software!
+      </Typography>
+      {loading ? (
+        <div className={classes.loadingWrapper}>
+          <CircularProgress size="6rem" />
+        </div>
+      ) : null}
+    </div>
+  );
 };
 
 export default Welcome;
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    container: {
+      flexGrow: 1,
+    },
+    loadingWrapper: {
+      width: "100vw",
+      height: "100vh",
+      position: "absolute",
+      top: 0,
+      left: 0,
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      zIndex: 100,
+      opacity: 0.5,
+      backgroundColor: "black",
+    },
+  })
+);
