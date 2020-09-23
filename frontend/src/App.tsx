3e2f5a17c4647ccc3a1710c0f86f3399d8b23b02
Callum Macdonald <git@callum-macdonald.com>
2020-09-23 15:05:36 +0200

    Import noty css and theme.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,8 @@
 import "fontsource-roboto";
 import React from "react";
 import Routes from "./scenes/Routes/Routes.scene";
+import "noty/lib/noty.css";
+import "noty/lib/themes/mint.css";
 
 function App() {
   return (
